a0a54436e8d070dd5664348372ebe6a3527f8fae
ju-zp <jhuntington@tutanota.com>
2020-05-08 13:44:04 +0100

    added handler and updated project structure

---
+++ svc/pinghandlers/ping.go
@@ -0,0 +1,11 @@
+package pinghandlers
+
+import (
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/ju-zp/tasker/gen/restapi/operations"
+)
+
+// GetPing returns the status of the server
+func GetPing(params operations.GetPingParams) middleware.Responder {
+	return operations.NewGetPingOK().WithPayload("Server is running")
+}
