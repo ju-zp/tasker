cb62adad7ee9e5b4c29654188ffc893d6141797a
ju-zp <jhuntington@tutanota.com>
2020-05-10 11:29:56 +0100

    added create todo handler

--- svc/todohandlers/todo.go
+++ svc/todohandlers/todo.go
@@ -7,8 +7,14 @@ import (
 	"github.com/ju-zp/tasker/gen/restapi/operations"
 )
 
-// GetTodo gets all todos
-func GetTodo(params *operations.GetTodoParams) middleware.Responder {
+// GetTodos gets all todos
+func GetTodos(params *operations.GetTodoParams) middleware.Responder {
 	fmt.Println("here")
 	return operations.NewGetTodoOK()
+}
+
+// CreateTodo makes a new todo entry in db
+func CreateTodo(params *operations.PostTodoParams) middleware.Responder {
+	fmt.Println("or here")
+	return operations.NewPostTodoOK()
 }
